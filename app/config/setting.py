d9c391034da9c56d6da19d83ecf6c6d53df8b6ef
yabokar1 <yabokar@gmail.com>
2024-04-24 13:14:23 -0400

    Updated setting.py

--- app/config/setting.py
+++ app/config/setting.py
@@ -3,10 +3,10 @@ class AppConfig:
     USER = os.environ.get('USER')
     PASSWORD = os.environ.get('PASSWORD')
     API_KEY = os.environ.get('OPEN_AI')
-    API_KEY = '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'
+    API_KEY = os.environ.get('API_KEY')
     ANALYZE_URL = os.environ.get('ANALYZE_URL')
     ANONYMIZE_URL = os.environ.get('ANONYMIZE_URL')
-    AWS_ACCESS_KEY_ID = 'AKIAY4ZAXZ366WHTJTC2'
-    AWS_SECRET_ACCESS_KEY = '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'
-    S3_BUCKET_NAME = 'pllbucket1'
-    OUTPUT_FILE = 'output.json'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
+    OUTPUT_FILE = os.environ.get('OUTPUT_FILE')
